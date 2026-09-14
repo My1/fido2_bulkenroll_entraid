56f748395fa15eb8efa7837a86a912e3a89eddc6
Maximilian Grüttemeier <m.gruettemeier@aperture-development.de>
2026-09-14 14:10:40 +0200

    Fix linking of the old tool to read fido serial keys

--- EnrollFIDO2_fido2-cred.ps1
+++ EnrollFIDO2_fido2-cred.ps1
@@ -622,9 +622,9 @@ $form.Controls.Add($btnProceed)
             if (-not $upn) { continue }
 			    $currentIndex++
 
-            # Fetch the serial number using read_serial_t2.exe
+            # Fetch the serial number using read_serial_t2.ps1
             try {
-                $serialOutput = (& ".\read_serial_t2.exe").Trim()
+                $serialOutput = (& ".\read_serial_t2.ps1").Trim()
                 if ([string]::IsNullOrWhiteSpace($serialOutput) -or $serialOutput -eq "None") {
                     [System.Windows.Forms.MessageBox]::Show("No valid serial number detected.`nSkipping.`nPlease prepare next user's key.", "Error")
                     Write-Host "No valid serial number for $upn. Skipping"
